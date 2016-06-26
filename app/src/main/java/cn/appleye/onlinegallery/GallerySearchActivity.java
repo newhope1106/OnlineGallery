d5511c586c2abe530e3ca8216413298b927393ad
newhope1106 <408705420@qq.com>
2016-06-26 22:16:40 +0800

    添加主列表界面和搜索图片界面布局

---
+++ app/src/main/java/cn/appleye/onlinegallery/GallerySearchActivity.java
@@ -0,0 +1,13 @@
+package cn.appleye.onlinegallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class GallerySearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_search);
+    }
+
+}
--- app/src/main/java/cn/appleye/onlinegallery/OnlineGalleryActivity.java
+++ app/src/main/java/cn/appleye/onlinegallery/OnlineGalleryActivity.java
@@ -1,16 +1,29 @@
 package cn.appleye.onlinegallery;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class OnlineGalleryActivity extends Activity {
+    private View mSearchImageOnlineView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_gallery);
+
+        mSearchImageOnlineView = findViewById(R.id.search_image_online_btn);
+        mSearchImageOnlineView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(OnlineGalleryActivity.this, GallerySearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
