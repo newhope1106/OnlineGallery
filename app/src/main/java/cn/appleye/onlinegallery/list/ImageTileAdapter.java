dd8fd9a62a890a1034e187eac2d76f4c6f23fa32
newhope1106 <408705420@qq.com>
2016-06-28 23:10:57 +0800

    添加图片列表adapter

---
+++ app/src/main/java/cn/appleye/onlinegallery/list/ImageTileAdapter.java
@@ -0,0 +1,32 @@
+package cn.appleye.onlinegallery.list;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by liuliaopu on 2016/6/28.
+ */
+public class ImageTileAdapter extends BaseAdapter {
+    private static final String TAG = "ImageTileAdapter";
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
