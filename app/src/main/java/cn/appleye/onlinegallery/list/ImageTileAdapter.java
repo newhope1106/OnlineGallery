3d76477468e22257e63b8afba1ac92eefbe5a5d9
newhope1106 <408705420@qq.com>
2016-06-30 23:03:01 +0800

    添加图片列表部分实现

--- app/src/main/java/cn/appleye/onlinegallery/list/ImageTileAdapter.java
+++ app/src/main/java/cn/appleye/onlinegallery/list/ImageTileAdapter.java
@@ -1,18 +1,46 @@
 package cn.appleye.onlinegallery.list;
 
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+import cn.appleye.onlinegallery.R;
 
 /**
  * Created by liuliaopu on 2016/6/28.
  */
 public class ImageTileAdapter extends BaseAdapter {
     private static final String TAG = "ImageTileAdapter";
+    private static int sNumberImagesOneRow = 4;
+
+    private Context mContext;
+    private LayoutInflater mInflater;
+
+    private ArrayList<Uri> mImageUriList = null;
+
+    public ImageTileAdapter(Context context) {
+        mContext = context;
+        mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        sNumberImagesOneRow = context.getResources().getInteger(R.integer.number_images_one_row);
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        if (mImageUriList == null) {
+            return 0;
+        }
+
+        if (sNumberImagesOneRow <= 0) {
+            return mImageUriList.size();
+        }
+
+        return (mImageUriList.size()+1)/sNumberImagesOneRow;
     }
 
     @Override
